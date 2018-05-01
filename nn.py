b96d6598535350bb1bbb9ab7811c0f8148be3ddf
koyo <koyo@koyo.kr>
2018-05-01 11:55:50 +0900

    add nn.py

--- nn.py
+++ nn.py
@@ -9,6 +9,18 @@ HIDDENNO = 2
 MAXINPUTNO = 100
 
 
+def f(u: float) -> float:
+    return 1 if u >= 0 else 0
+    # return 1 / (1 + exp(-u))
+
+
+def forward(wh: Tuple[Tuple[float, float], ...],
+            wo: Tuple[float, float, float],
+            e: Tuple[float, float]) -> float:
+    hi = tuple(f(sum(x*y for x, y in zip(w, e)) - w[INPUTNO]) for w in wh)
+    return f(sum(x*y for x, y in zip(hi, wo)) - wo[HIDDENNO])
+
+
 def getdata() -> Tuple[Tuple[float, float], ...]:
     return tuple(tuple(float(s) for s in line.split(' ')[:INPUTNO]) for line in stdin)
 
@@ -27,7 +39,8 @@ def main() -> None:
     e = getdata()
     n_of_e = len(e)
     print('number of data: %d' % n_of_e)
-
+    for i, g in enumerate(e):
+        print(i, ' '.join('%lf' % x for x in g), '%lf' % forward(wh, wo, g))
 
 
 if __name__ == '__main__':
